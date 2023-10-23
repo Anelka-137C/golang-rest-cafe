09f147a54ee50fbaa28046896a4edb98a093d9fc
Casas Parra <nacasasp@indracompany.com>
2023-10-23 14:13:15 -0500

    Some changes

--- src/middlewares/product.go
+++ src/middlewares/product.go
@@ -1,9 +1,7 @@
 package middlewares
 
 import (
-	"log"
 	"net/http"
-	"os"
 	"time"
 
 	"github.com/Anelka-137C/cafe-app/internal/product"
@@ -11,10 +9,8 @@ import (
 
 	"github.com/Anelka-137C/cafe-app/src/helpers"
 	"github.com/Anelka-137C/cafe-app/src/util"
-	"github.com/dgrijalva/jwt-go"
 
 	"github.com/go-playground/validator/v10"
-	"github.com/joho/godotenv"
 )
 
 func ValidateCategory(db product.Repository) validator.Func {
@@ -27,23 +23,11 @@ func ValidateCategory(db product.Repository) validator.Func {
 
 func ValidateJwtForUsers() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		claims := jwt.MapClaims{}
+
 		date := time.Now()
 		token := c.GetHeader("token")
-		err := godotenv.Load()
-		if err != nil {
-			log.Fatal("Error loading .env file")
-		}
-		sampleSecretKey := os.Getenv(secretKey)
-		_, err = jwt.ParseWithClaims(token, claims, func(token *jwt.Token) (interface{}, error) {
-			return []byte(sampleSecretKey), nil
-		})
-		if err != nil {
-			errors := helpers.GenerateOneError("token", "You must send this field")
-			util.BuildBadResponse(http.StatusBadRequest, errors, c)
-			c.Abort()
-			return
-		}
+
+		claims := util.ExtractClaimsFromJwt(token, c)
 		if !claims.VerifyExpiresAt(date.UnixMilli(), true) {
 			errors := helpers.GenerateOneError("token", "Session expired")
 			util.BuildBadResponse(http.StatusBadRequest, errors, c)
--- src/util/generic.go
+++ src/util/generic.go
@@ -1,8 +1,19 @@
 package util
 
 import (
+	"net/http"
+	"os"
+
 	"github.com/Anelka-137C/cafe-app/internal/domain"
+	"github.com/Anelka-137C/cafe-app/src/helpers"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+const (
+	secretKey = "SECRET_KEY"
+	adminRole = "ADMIN_ROLE"
 )
 
 func BuildResponse(status int, object interface{}, message string, c *gin.Context, entity string) {
@@ -31,4 +42,28 @@ func BuildResponse(status int, object interface{}, message string, c *gin.Contex
 
 func BuildBadResponse(status int, err []domain.ErrorMsg, c *gin.Context) {
 	c.JSON(status, gin.H{"error": err})
+}
+
+func ExtractClaimsFromJwt(tokenString string, c *gin.Context) *jwt.MapClaims {
+	claims := jwt.MapClaims{}
+	token := c.GetHeader("token")
+	err := godotenv.Load()
+	if err != nil {
+		errors := helpers.GenerateOneError("env", "Error loading .env file")
+		BuildBadResponse(http.StatusBadRequest, errors, c)
+		c.Abort()
+	}
+	sampleSecretKey := os.Getenv(secretKey)
+	_, err = jwt.ParseWithClaims(token, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(sampleSecretKey), nil
+	})
+
+	if err != nil {
+		errors := helpers.GenerateOneError("token", "You must send this field")
+		BuildBadResponse(http.StatusBadRequest, errors, c)
+		c.Abort()
+		return &claims
+	}
+
+	return &claims
 }
